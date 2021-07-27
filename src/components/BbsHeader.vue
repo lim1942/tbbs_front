d6e3f09b6e84fdd7221b52bd59846d4c6a24b20d
lim <you@example.com>
2021-07-28 00:47:19 +0800

    first

---
+++ src/components/BbsHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="4">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content">留言板</div>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="4">
+      <div v-if="!info" class="grid-content">登录/注册</div>
+      <div v-else class="grid-content">你好！{{info.a}}/{{info.b}} 注销</div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'BbsHeader',
+  props: {
+    info: {
+      default: {a: 1, b: 2}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.grid-content {
+  min-height: 36px;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-container>
+    <!--侧边栏-->
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>
+        <BbsHeader></BbsHeader>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import BbsHeader from './BbsHeader'
+
+export default {
+  components: {
+    'BbsHeader': BbsHeader
+  },
+  name: 'index'
+}
+</script>
+
+<style scoped>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  height: 100%
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 100vh;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 100%;
+}
+</style>
